e881dac70cc5c5e51818819c3130634fd78e290d
NagKothapalli <nag022@gmail.com>
2023-05-12 22:13:10 +0530

    May12th code pushed

---
+++ src/coreConcepts/ArrayOperations.java
@@ -0,0 +1,6 @@
+package coreConcepts;
+
+public class ArrayOperations
+{
+
+}
--- src/coreConcepts/StringOperations.java
+++ src/coreConcepts/StringOperations.java
@@ -8,7 +8,7 @@ public class StringOperations {
 	String b = "JAVA";
 	String c = "Selenium With JavA                 ";
 
-	// length , toLowerCase , toUpperCase , trim , contains
+	// length , toLowerCase , toUpperCase , trim , contains , equalsIgnoreCase , equals , chatAt
 	@Test
 	public void stringFunctions() {
 		int len = a.length();
@@ -50,5 +50,103 @@ public class StringOperations {
 	}
 	
 	//charAt
-
+	@Test
+	public void characterFunction()
+	{
+		String str = "    Selenium With Java           ";
+		System.out.println("Length of the string :"+str.length());
+		System.out.println("4th char :" + str.trim().charAt(3));
+		
+	}
+	@Test
+	public void findCharacter() //m
+	{
+		String str = "    Selenium With Java           ";
+		String str2 = str.trim();
+		int len = str2.length();
+		for(int i=0;i<len;i++)
+		{
+			char mychar = str2.charAt(i);
+			System.out.println("all chars :" + mychar);
+			if(mychar == 'm')
+			{
+				System.out.println("Found the Character");
+				break;
+			}
+		}
+	}
+	@Test
+	public void findCharacter_test()
+	{
+		findCharacter("Selenium With CSharp",'W');
+		findCharacter("Selenium With CSharp",'S');
+		findCharacter("Selenium With CSharp",'h');
+		findCharacter("Selenium With CSharp",'x');
+	}
+	public void findCharacter(String str,char chartoFind) //m
+	{
+		boolean flag = false;
+		//String str = "    Selenium With Java           ";
+		String str2 = str.trim();
+		int len = str2.length();
+		for(int i=0;i<len;i++)
+		{
+			char mychar = str2.charAt(i);
+			//System.out.println("all chars :" + mychar);
+			if(mychar == chartoFind)
+			{
+				flag = true;
+				break;
+			}
+		}
+		if(flag) {
+			System.out.println("Found the Character :" + chartoFind);
+		}
+		else {
+			System.out.println("Did not Found the Character :" + chartoFind);
+		}
+	}
+	@Test
+	public void findCharacterCount()
+	{
+		String str = "Selenium With Java";
+		int counter = 0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i) == 'i')
+			{
+				counter++; // counter = counter+1;
+			}
+		}
+		System.out.println("Count of char a :" + counter);
+	}
+	@Test
+	public void subStringOperation()
+	{   //            01234
+		String str = "Selenium With Java";		
+		String s8 = str.substring(8);
+		System.out.println("string from 8th char :" + s8);
+		String s08 = str.substring(0, 8);
+		System.out.println("string from 0-8 :" +s08 );
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
