fcb4a4439287b068495a67995e0b5513e3aa3176
NagKothapalli <nag022@gmail.com>
2023-06-05 23:07:51 +0530

    June5th code pushed

--- src/seleniumPractice/GmailAutomation.java
+++ src/seleniumPractice/GmailAutomation.java
@@ -1,10 +1,14 @@
 package seleniumPractice;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
 
 public class GmailAutomation
 {
+	String expectedTitle = "Gmail";
 	@Test
 	public void launchGmailApplication()
 	{
@@ -16,6 +20,21 @@ public class GmailAutomation
 		System.out.println("My window session ID :" + session);
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
+		//Validation : Comparing the expected value with the actual value 
+		String actualTitle = driver.getTitle();
+		
+		if(expectedTitle.equals(actualTitle)) {
+			System.out.println("Launch Application is Successfull");
+		}
+		else {
+			System.out.println("Launch Application is Failed");
+		}
+		Assert.assertEquals(expectedTitle, actualTitle);
+		driver.findElement(By.id("identifierId")).sendKeys("nag022");
+	}
+	public void loginApplication()
+	{
+		
 	}
 	
 
