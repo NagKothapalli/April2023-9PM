e5323362e7392d022ba0ac9f2fd50e3311bad83f
NagKothapalli <nag022@gmail.com>
2023-06-16 22:54:36 +0530

    June16th code pushed

--- src/seleniumPractice/ApsrtcAutomation.java
+++ src/seleniumPractice/ApsrtcAutomation.java
@@ -1,15 +1,24 @@
 package seleniumPractice;
 
+import java.time.Duration;
+
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class ApsrtcAutomation
 {
 	ChromeDriver driver;
+	Actions actions;
 	public ApsrtcAutomation()
 	{
 		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\JarFiles\\chromedriver-win32-90\\chromedriver.exe");
 		driver = new ChromeDriver();//it will open an empty google chrome browser //1234
+		actions = new Actions(driver);
 	}
 	String expectedTitle = "Gmail";
 	//ChromeDriver : get , getWindowHandle , getTitle , getCurrentUrl , findElement , findElements , 
@@ -19,7 +28,51 @@ public class ApsrtcAutomation
 	public void launchGmailApplication()
 	{
 		System.out.println("Testcase : Launch Gmail Application");		
-		driver.get("https://gmail.com"); 
+		driver.get("https://www.apsrtconline.in/"); 
 	}
+	@Test
+	public void bookBusTicket()
+	{
+		System.out.println("Testcase : Book Bus Ticket");		
+		driver.findElement(By.xpath("//input[@name='source']")).sendKeys("HYDERABAD");
+		actions.pause(Duration.ofSeconds(1)).sendKeys(Keys.ENTER).build().perform();
+		driver.findElement(By.xpath("//input[@name='destination']")).sendKeys("GUNTUR");
+		actions.pause(Duration.ofSeconds(1)).sendKeys(Keys.ENTER).build().perform();
+		driver.findElement(By.xpath("//input[@name='txtJourneyDate']")).click();
+		driver.findElement(By.xpath("//a[text()='22']")).click();
+		driver.findElement(By.xpath("//input[@name='searchBtn']")).click();
+	}
+	@Test
+	public void handleAlerts()
+	{
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@name='source']")).sendKeys("HYDERABAD");
+		actions.sendKeys(Keys.TAB).build().perform();
+		driver.findElement(By.xpath("//input[@name='searchBtn']")).click();
+		driver.switchTo().alert().accept();
+		driver.findElement(By.xpath("//input[@name='source']")).clear();
+		driver.findElement(By.xpath("//input[@name='source']")).sendKeys("HYDERABAD");
+		actions.pause(Duration.ofSeconds(1)).sendKeys(Keys.ENTER).build().perform();
+		driver.findElement(By.xpath("//input[@name='searchBtn']")).click();
+	}
+	
+	@Test
+	public void mouseOPerations()
+	{
+		driver.manage().window().maximize();
+		WebElement element = driver.findElement(By.xpath("//input[@name='source']"));
+		actions.moveToElement(element).sendKeys("HYDERABAD").doubleClick().contextClick().build().perform();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
